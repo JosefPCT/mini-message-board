49e86359ba8d9797199a8a1774d655b6b992279f
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-10-07 14:03:09 +0800

    feat: add a way to create new message and display it on the board

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,10 +1,12 @@
+const messages = require('../models/messages');
+
 module.exports = {
   renderNew: (req, res) => {
     res.render('pages/new', { title: 'New Message' });
   },
   createMessage: (req, res) => {
-    console.log(req.body);
-
-    res.end()
+    // console.log(req.body);
+    messages.push({ text: req.body.messageText, user: req.body.author, added: new Date() });
+    res.redirect('/');
   }
 }
