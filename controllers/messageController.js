36f2892bdfd19c9de5814604294150055dbb0e1e
Josef Tinagan <tinaganjosefperry@gmail.com>
2025-10-08 09:55:38 +0800

    feat: refactor messageController to use a helper function to get the message by id

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -1,38 +1,18 @@
 const messages = require('../models/messages');
 
+const renderMessage = (req, res) => {
+  let parsedId = parseInt(req.params.id);
+  let mes = getMessageById(parsedId);
+  res.render('pages/message', { 
+      title: 'Message',
+      message: mes
+  })  
+}
+
+const getMessageById = (id) => {
+  return messages.find((message) => id === message.id);
+}
+
 module.exports = {
-//   renderMessage: (req ,res) => {
-//     // console.log(req.params.ids);
-//     // console.log(messages.find((message) => req.params.id === message.id));
-//     let parsedId = parseInt(req.params.id);
-//     console.log(parsedId);
-//     console.log(typeof parsedId);
-//     let mes = messages.find((message) => {
-//         // console.log(typeof req.params.id);
-//         // console.log(typeof message.id);
-//         // let parsedId = parseInt(req.params.id);
-//         // console.log(parsedId);
-//         // console.log(message.id);
-
-//         return parsedId === message.id;
-//     });
-//     console.log(mes);
-//   },
-
-  renderMessage: (req ,res) => {
-    let parsedId = parseInt(req.params.id);
-    let mes = messages.find((message) => {
-
-        return parsedId === message.id;
-    });
-    res.render('pages/message', { 
-        title: 'Message',
-        message: mes
-    })
-  },
-
-  getMessageById: (id) => {
-    // return messages.find((message) => id === message.id);
-    console.log(messages.find((message) => id === message.id));
-  }
+  renderMessage
 };
